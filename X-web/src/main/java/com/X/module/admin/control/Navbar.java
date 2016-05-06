27bb5cafdfb56be943f05ae553d93faf51529f62
donahue-ldz <ldz2012yn@gmail.com>
2016-05-06 11:44:51 +0800

    分模块系统

---
+++ X-web/src/main/java/com/X/module/admin/control/Navbar.java
@@ -0,0 +1,21 @@
+package com.X.module.admin.control;
+
+import com.X.common.BaseScreen;
+
+/**
+ * Created by dezhao.ldz on 11/19/15.
+ * Email : dezhao.ldz@alibaba-inc.com
+ */
+public class Navbar extends BaseScreen {
+//
+//    @Override
+//    protected WebResult handleRequest(Context context) throws Exception {
+//        return null;
+//    }
+//
+//    @Override
+//    protected List<String> getPermissionList() throws AmsOpsBizException {
+//        return null;
+//    }
+}
+
