b99418289371ce05444e58d16a811734c562fd4e
zhong.zhou <zhong.zhou@kevin.local>
2016-05-15 13:41:46 +0800

    modify the name of SDAOException

--- X-dal/src/main/java/com/X/dal/exception/XDAOException.java
+++ X-dal/src/main/java/com/X/dal/exception/XDAOException.java
@@ -3,19 +3,19 @@ package com.X.dal.exception;
 /**
  * Created by donahue on 5/6/16.
  */
-public class XDaoException extends Exception{
+public class XDAOException extends Exception{
 
     private static final long serialVersionUID = 2198076290841334919L;
 
-    public XDaoException() {
+    public XDAOException() {
         super();
     }
 
-    public XDaoException(String message, Throwable cause) {
+    public XDAOException(String message, Throwable cause) {
         super(message, cause);
     }
 
-    public XDaoException(String message) {
+    public XDAOException(String message) {
         super(message);
     }
 }
