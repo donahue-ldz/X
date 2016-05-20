5aac1f5f69e6338613c173dc7b3c277a1b2f8dd4
donahue-ldz <ldz2012yn@gmail.com>
2016-05-20 19:37:03 +0800

    update

--- X-biz/src/main/java/com/X/biz/aggregation/BBSVIewAgg.java
+++ X-biz/src/main/java/com/X/biz/aggregation/BBSVIewAgg.java
@@ -1,8 +1,12 @@
 package com.X.biz.aggregation;
 
+import com.X.biz.bbs.vo.BBSView;
+import com.X.biz.exception.XException;
+
 /**
  * @author donahue ldz2012yn@gmail.com
  * @create 2016-05-19 10:21 PM
  **/
-public interface BBSVIewAgg {
+public interface BBSViewAgg {
+    BBSView view() throws XException;
 }
